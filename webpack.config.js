ff3cfd70402ed373d6f975554850feaf4eac1b55
k-nagama <k.nagama0632@gmail.com>
2023-03-29 12:14:24 +0900

    min.js にライセンス情報などを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+// package.json の情報を min.js のコメントに追加
+const packageJson = require('./package.json');
+const version = packageJson.version
+const authorName = packageJson.author.name;
+const authorEmail = packageJson.author.email;
 
 module.exports = {
   mode: 'production',
@@ -8,4 +16,17 @@ module.exports = {
     filename: 'ipcheckersan.min.js',
     libraryTarget: 'umd',
   },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `ip-checker-san v` + version + `
+
+(c) 2023 ` + authorName + ` <` + authorEmail + `>
+License: MIT`,
+    }),
+  ],
 };
